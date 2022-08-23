ca54b85b15f5ff34321333a0518a98036da61f5c
Adam Cox <adamcox.dev@gmail.com>
2022-08-23 12:27:53 -0600

    Commit after objects portion of lab

--- index.js
+++ index.js
@@ -4,3 +4,46 @@ let person = {
     lastName: 'Cox',
     age: 32
 }
+
+console.log(person.firstName);
+console.log(person['lastName']);
+
+//destructuring 
+let meal = {
+    appetizer: 'chips and salsa',
+    entree: 'steak and bean burrito',
+    dessert: 'fried ice cream',
+    drink: 'coke'
+}
+
+//let dessert = meal.dessert
+let {dessert,entree,appetizer,drink: bestSodaEver} = meal;
+
+//let {entree, appetizer} = meal;
+
+//let {drink: bestSodaEver} = meal;
+
+//console.log(appetizer, entree, dessert);
+//console.log(bestSodaEver);
+//console.log(meal);
+
+
+//For in loops!!
+for(let key in meal){
+ console.log(`For my ${key}} I had ${meal[key]}.`);
+}
+
+
+//Adding,changing and deleting properties from objects
+person.occupation = 'Student at DevMountain'; //adding
+person.age = 33; //or person.age++ //changing
+delete person.lastName; //deleting 
+
+console.log(person);
+
+
+
+
+
+
+
