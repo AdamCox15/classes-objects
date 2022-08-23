0c68ddcd83e67d4371c0fbcbe7f827a37fa14334
Adam Cox <adamcox.dev@gmail.com>
2022-08-23 13:07:01 -0600

    Final commit in lab after classes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ let person = {
     age: 32
 }
 
-console.log(person.firstName);
-console.log(person['lastName']);
+//console.log(person.firstName);
+//console.log(person['lastName']);
 
 //destructuring 
 let meal = {
@@ -30,7 +30,7 @@ let {dessert,entree,appetizer,drink: bestSodaEver} = meal;
 
 //For in loops!!
 for(let key in meal){
- console.log(`For my ${key}} I had ${meal[key]}.`);
+ //console.log(`For my ${key}} I had ${meal[key]}.`);
 }
 
 
@@ -39,11 +39,58 @@ person.occupation = 'Student at DevMountain'; //adding
 person.age = 33; //or person.age++ //changing
 delete person.lastName; //deleting 
 
-console.log(person);
+//console.log(person);
 
 
 
+//Classes and extending classes
+class Pet{
+  constructor(name, age, breed, color){
+    this.name = name,
+    this.age = age,
+    this.breed = breed,
+    this.color = color
+  }
+}
 
+let tuukka = new Pet('Tuukka', 6, 'Doberman', 'Red and Rust');
+let rook = new Pet('Rook', 1, 'Belgian Malinois', 'Black and Tan');
+let dekker = new Pet('Dekker', '8 months', 'Rottweiler', 'Black and Tan');
 
+/*console.log(tuukka.name);
+console.log(tuukka);
+console.log(rook.name);
+console.log(dekker);
+console.log(dekker.age);*/
+console.log(rook.breed);
+console.log(tuukka.color);
 
+class Dog extends Pet {
+    constructor(name, age, breed){
+        super(name, age)
+        this .breed = breed
+    }
+    bark(){
+        console.log(`Bark, my name is ${this.name} and I am a ${this.breed}.`)
+    }
+}
+let arrow = new Dog('Arrow', 5, 'Shepard/Husky mix');
+console.log(arrow);
+arrow.bark();
 
+class Hyena extends Pet{
+constructor(name, age, color){
+    super(name,age)
+    this.color = color,
+    this.temperament = 'mean'
+}
+beNice(){
+    this.temperament = 'nice'
+    console.log(`${this.name} is now a nice hyena!`)
+}
+
+}
+let kane = new Hyena('Kane', 3, 'spotted')
+
+console.log(kane.temperament);
+kane.beNice();
